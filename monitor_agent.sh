5360b4eda271c6a205699cc8b0845d99d2966edb
januslo6211@icloud.com <januslo6211@icloud.com>
2017-08-18 23:26:56 +0800

    limits the upload count
    limits the upload count while the processor of the server seams dead.

--- monitor_agent.sh
+++ monitor_agent.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 #The agent of the monitor, for generating the information file and transfers to the specified SFTP location.
 #Author: Janus J K Lu
 #Date 2017-08-17
@@ -23,19 +23,36 @@ upload_path="";
 #server_desc: the short description the the current server
 server_desc="";
 
-working_dir="/root/monitor";
+#Limits the existing file count, we see the processor of server is death if the max_exist_file_count reached.
+max_exist_file_count=100;
+
+#need to enter the working dir manually in the Cron job.
+working_dir="";
 
 cd $working_dir;
 
 current_time=$(date '+%Y_%m_%d_%H_%M');
 file_name=$current_time"_"$file_name_suffix;
+tem_file_name="tem_"$file_name;
+lftp sftp"://"$sftp_user":"$sftp_password"@"$sftp_host -e "cd "$upload_path";ls;bye;">>$tem_file_name;
+exist_count=$(cat $tem_file_name | grep '^-' | grep -E '*'$file_name_suffix | wc -l);
+rm $tem_file_name;
+if [ -n "$exist_count" ] && [ $exist_count -gt $max_exist_file_count ];then
+echo 'reach max exist file count:'$max_exist_file_count', actully got count: '$exist_count', exit.'
+exit 0;
+fi
+#echo "exist_count:"$exits_count;
+#rm $tem_file_name;
 echo "[SERVER_DESC]\n"$server_desc"\n">>$file_name;
 
 $working_dir"/system_info.sh" $file_name;
+#echo 1;
 #comment it if no docker in the server.
 $working_dir"/docker_status.sh" $file_name;
+#echo 2;
 
-lftp sftp"://"$sftp_user":"$sftp_password"@"$sftp_host -e "cd "$upload_path";put "$file_name";bye";
+lftp sftp"://"$sftp_user":"$sftp_password"@"$sftp_host -e "cd "$upload_path";put "$file_name";bye;";
 
 rm $file_name;
 echo "DONE";
+
