b078d05ce6e72484ec82f1232a9d911e17a215a0
januslo6211@icloud.com <januslo6211@icloud.com>
2017-08-21 19:13:47 +0800

    add host to mysql information
    add host to mysql information

--- mysql_status.sh
+++ mysql_status.sh
@@ -4,6 +4,7 @@ mysql_url="";
 mysql_user="";
 mysql_pwd="";
 echo "[MYSQL GLOBAL]\n">>$out_file_name;
+echo "mysql_host "$mysql_url>>$out_file_name;
 mysql -h $mysql_url -u $mysql_user --password=$mysql_pwd -e  " show global status;">>$out_file_name;
 echo "\n[END MYSQL GLOBAL]\n">>$out_file_name;
 echo "[MYSQL VARIABLES]\n">>$out_file_name;
